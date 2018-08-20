d3cd8e3e38f803647c19fab20451981feb3a3d84
ebittyschwinnie <elizabeth.m.clarkson@gmail.com>
2018-08-19 17:59:33 -0700

    tables populating

--- store.js
+++ store.js
@@ -1,8 +1,7 @@
-function generateRandom(min, max) {
-  return Math.floor(Math.random() * (max - min +1)) + min;
-}
+//function generateRandom(min, max) {
+//  return Math.floor(Math.random() * (max - min +1)) + min;
+//}
 
-var hours = ["10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM"]
 
 //  var location = new Object ();
 //    location.name = 'Pioneer Square';
@@ -14,101 +13,128 @@ var hours = ["10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", 
 //        var amountOfCookies = ((customersHour) * (this.avgCookieSale))
 //        return parseInt(amountOfCookies);
 //      }
-
+console.log('here')
 function Store(name, minCust, maxCust, avgCookieSale) {
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgCookieSale = avgCookieSale;
   this.total = 0;
-  this.cookiesPerHour = function () {
-    var customersHour = generateRandom(this.minCust, this.maxCust);
+  this.hours = ["10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM"];
+  this.generateRandom = function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust +1)) + this.minCust;
+  };
+  this.cookiesPerHour = function() {
+    var customersHour = this.generateRandom();
     var amountOfCookies = ((customersHour) * (this.avgCookieSale))
+    return parseInt(amountOfCookies);
+
   };
+
   this.buildTable = function() {
-    var container = document.getElementById('table-container');
-    var table = document.getElementById('table');
-    table.setAttribute("border", "1");
-    this.header(table);
-    this.tableHours(table);
-    this.totalRow(table);
-    this.appendChild(table);
+    var locations = document.getElementById("locations");
+    var table = document.createElement('table');
+    var tbody = document.createElement('tbody');
+    tbody.setAttribute('id',this.name);
+    var row = document.createElement('tr');
+    var theader = document.createElement('th');
+    theader.innerText = this.name;
+    row.appendChild(theader);
+    //theader = document.createElement('th');
+  //  theader.innerText = 'Hour';
+    //row.appendChild(theader);
+    tbody.appendChild(row);
+    var row = document.createElement('tr');
+    var theader = document.createElement('th');
+    theader.innerText = 'Hours'
+    row.appendChild(theader);
+    var theader = document.createElement('th');
+    theader.innerText = 'Cookies'
+    row.appendChild(theader);
+    tbody.appendChild(row);
+    table.appendChild(tbody);
+    locations.appendChild(table);
   };
 
+  this.createData = function() {
+    var findTBody1 = document.getElementById(this.name);
+    //var findTBody2 = findTBody1.getElementById('tbody')[0];
 
+    for(var hoursIndex = 0; hoursIndex <this.hours.length; hoursIndex++) {
+      var generateRandom = this.cookiesPerHour();
+      this.total += generateRandom;
+      row = document.createElement('tr');
+      tdata = document.createElement('td');
+      tdata.innerText = this.hours[hoursIndex];
+      row.appendChild(tdata);
+      tdata = document.createElement('td');
+      tdata.innerText = generateRandom;
+      row.appendChild(tdata);
+      findTBody1.appendChild(row);
+    }
+      row = document.createElement('tr');
+      cell = document.createElement('th');
+      cell.innerText = 'Total';
+      row.appendChild(cell);
+      cell = document.createElement('th');
+      cell.innerText = this.total;
+      row.appendChild(cell);
+      findTBody1.appendChild(row);
 
-var pioneerLocation = new Store('Pioneer Square', 17, 88, 5.2);
-var airportLocation = new Store('Portland Airport', 6, 24, 1.4);
-var washingtonLocation = new Store('Washington Square', 11, 38, 1.9);
-var sellwoodLocation = new Store('Sellwood', 20, 48, 3.3);
-var pearlLocation = new Store('Pearl District', 3, 24, 2.6);
+  };
 
-
-
-//var location1 = {
-//  name: 'Pioneer Square',
-//  minCust: 17,
-//  maxCust: 88,
-//  avgCookieSale: 5.2,
-//  total: 0,
-//  toHTML: function() {
-//    var HTML = '<ul>'+ location1.name;
-//  for (var cookieTime = 0; cookieTime < hours.length; cookieTime++) {
-//    var cookiesMade = location1.cookiesPerHour()
-//    location1.total += cookiesMade
-//    console.log('total cookies made' + location1.total )
-//    HTML += "<li>" + hours[cookieTime] + " : " + cookiesMade + " sold/hour </li>";
-//  }
-//    HTML += "TOTAL: " + location1.total + '</ul>';
-//  return HTML;
-//  },
-//    cookiesPerHour: function() {
-//    var customersHour = generateRandom(location1.minCust, location1.maxCust)
-//    var amountOfCookies = ((customersHour) * (location1.avgCookieSale))
-//    return parseInt(amountOfCookies);
-//  }
-//}
-
-
-var locations = [
-  pioneerLocation,
-  airportLocation,
-  washingtonLocation,
-  sellwoodLocation,
-  pearlLocation,
-];
-
-//method that also builds out table for me in the constructor function
-
-function buildList() {
-   var list = document.getElementById('locations');
-   for (var index = 0; index < locations.length; index++) {
-     var storeLocation = locations[index];
-     // list.innerHTML += locations.toHTML();
-     header(storeLocation);
-   }
- }
-
-header = function (store) {
-  var table = document.getElementById('locations');
-  var row = document.createElement('tr');
-  var heading = document.createElement('th');
-  heading.setAttribute("colspan", "2");
-  heading.innerText = (store.name);
-  row.appendChild(heading);
-  table.appendChild(row);
 }
 
 
-//buildTable = function() {
-  //var container = document.getElementById('table-container');
-  //var table = document.getElementById('table');
-  //table.setAttribute("border", "1");
-  //this.header(table);
-  //this.tableHours(table);
-  //this.totalRow(table);
-  //this.appendChild(table);
+var stores = [];
+
+stores.push(new Store('Pioneer Square', 17, 88, 5.2));
+stores.push(new Store('Portland Airport', 6, 24, 1.4));
+stores.push(new Store('Washington Square', 11, 38, 1.9));
+stores.push(new Store('Sellwood', 20, 48, 3.3));
+stores.push(new Store('Pearl District', 3, 24, 2.6));
+console.log(stores);
+
+for(var locationsIndex = 0; locationsIndex < stores.length; locationsIndex++) {
+  stores[locationsIndex].cookiesPerHour();
+  stores[locationsIndex].buildTable();
+  stores[locationsIndex].createData();
+}
+
+
+
+
+//method that also builds out table for me in the constructor function
+
+//function buildList() {
+  // var list = document.getElementById('locations');
+  // for (var index = 0; index < locations.length; index++) {
+    // var storeLocation = locations[index];
+     // list.innerHTML += locations.toHTML();
+    // header(storeLocation);
+  // }
+// }
+
+//header = function (store) {
+//  var table = document.getElementById('locations');
+//  var row = document.createElement('tr');
+//  var heading = document.createElement('th');
+//  heading.setAttribute("colspan", "2");
+//  heading.innerText = (store.name);
+//  row.appendChild(heading);
+//  table.appendChild(row);
 //}
 
 
-buildList();
+//buildTable = function() {
+//  var container = document.getElementById('table-container');
+//  var table = document.getElementById('table');
+//  table.setAttribute("border", "1");
+//  this.header(table);
+//  this.tableHours(table);
+//  this.totalRow(table);
+//  this.appendChild(table);
+//}
+
+
+//buildList();
